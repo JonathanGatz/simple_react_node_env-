6bcd1c15e955934563b29398ecf00f4ca398f39c
Jonathan Gatz <J.Gatz712@gmail.com>
2022-12-20 13:01:20 -0500

    finalize api fetch

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
+function App() {
+  const [backendData, setBackendData] = useState([{}]);
 
-function App(props) {
+  useEffect(() => {
+    fetch("/api").then(
+      response => response.json()
+      ).then(
+        data => {
+        setBackendData(data);
+      }
+      );
+  }, []);
+
   return (
     <div>
-
-      <h1>Hello Joanthan</h1>
-      
+      {(typeof backendData.users === "undefined") ? (
+        <p>Loading...</p>
+      ) : (
+        backendData.users.map((user, i) => (
+        <p key={i}>{user}</p>
+      ))
+    )}
     </div>
-  );
+  )
 }
 
 export default App;
--- server/server.js
+++ server/server.js
@@ -1,2 +1,9 @@
 const express = require('express') 
 const app = express()
+
+app.get('/api', (req, res) => {
+    res.json({"users": ["userOne", "userTwo", "userThree"]})
+})
+
+
+app.listen(5001, () => {console.log('server live on port 5000')})
